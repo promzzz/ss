ebc83c41bce0e4545b8024bb058c0b69aa518e5d
JeFF.Hou <cn.JeFF.H@gmail.com>
2018-04-18 09:45:28 +0800

    系统设定及账户添加方面的修改

--- MainEngine.py
+++ MainEngine.py
@@ -1,6 +1,5 @@
-import os, sys
+import os, sys, json
 from PyQt5.QtWidgets import QApplication
-
 
 from UserEngine import UserCenter
 from DataEngine import DataEngine
@@ -16,6 +15,45 @@ class MainEngine:
     # 主引擎，负责行情、时钟、实例、主界面
 
     def __init__(self, tzinfo=None):
+
+        string = input('是否添加新账户(y/n):')
+        if string == 'y':
+            user = {}
+            user['userid'] = input('请输入账号:')
+            user['mode'] = input('是否为实盘操控(y/n):')
+            if user['mode'] == 'y':
+                user["jobber"] = input('请输入服务商:')
+                user["password"] = input('请输入登录密码:')
+                user["type"] = input('请确定操控模式(web/client):')
+                user["uname"] = input('请输入用户姓名:')
+                user["path"] = input('请输入程序路径:')
+
+            while True:
+                user["brokerage"] = input('请确定手续费:')
+                try:
+                    user["brokerage"] = float(user["brokerage"])
+                    if type(user["brokerage"]) == float: break
+                except:
+                    print('您的输入不正确，请重新输入数字!')
+
+            while True:
+                user["stamptax"] = input('请确定印花税:')
+                try:
+                    user["stamptax"] = float(user["stamptax"])
+                    if type(user["stamptax"]) == float: break
+                except:
+                    print('您的输入不正确，请重新输入数字!')
+
+            user["autoRun"] = input('是否自动运行(y/n):')
+
+            user['mode'] =  True if user["mode"] == 'y' else False
+            user["autoRun"] = True if user["autoRun"] == 'y' else False
+
+            users.append(user)
+
+            uf = open(Configure+'users.json','w')
+            uf.write(json.dumps(users))
+            uf.close()
 
         if not os.path.exists(imgdir): os.makedirs(imgdir)
         if not os.path.exists(logdir): os.makedirs(logdir)
